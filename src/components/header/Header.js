7ba60302ec3879ac9e94b40770ee572e6f071c7a
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2024-02-11 22:09:29 +0530

    Header and Sidebar added to youtube screen

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './_header.scss';
 
-function Header() {
+import { FaBars } from "react-icons/fa"
+import { AiOutlineSearch } from "react-icons/ai"
+import { MdNotifications, MdApps } from "react-icons/md"
+
+function Header({handleToggleSidebar}) {
+
+
+
   return (
-    <div>Header</div>
+    <div className="border border-dark header">
+      <FaBars className="header-menu" size={26}  onClick={() => handleToggleSidebar()}/>
+      <img
+        src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png"
+        alt=""
+        className="header-img"
+      />
+      <form >
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <AiOutlineSearch size={20} />
+        </button>
+      </form>
+      <div className="header-icons">
+        <MdNotifications size={20} />
+        <MdApps size={20} />
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png"
+          alt="avatar"
+          className='user-icon'
+        />
+      </div>
+    </div>
   )
 }
 
